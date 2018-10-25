07ea26b0e9e0465308adc3f472e6ac099e4aedfd
Luis Conde <lconde@e-bitware.com>
2018-10-25 14:16:47 -0500

    [Estable] creacion de documentacion en funciones

--- export/user.js
+++ export/user.js
@@ -1,10 +1,18 @@
 module.exports = {
     fileName: 'user',
-    rawQuery: 'SELECT * FROM usuario;SELECT * FROM algo',
-    /* transform: function(result) {
-        
-        return { name: '', apellido: '' };
-    }, */
+    rawQuery: 'SELECT * FROM usuario;SELECT * FROM acreditado;',
+    transform: function(rows) {
+        let users = rows[0];
+        let acre = rows[1];
+
+        let count = 0;
+        for(let item of users) {
+            item.usu_nombre += ' ' + (acre[count].acr_nombre ? acre[count].acr_nombre : 'No tiene prro');
+            item.noContrato = 'C00' + count;
+            count++;
+        }
+        return users;
+    },
     fields: [{
         sourceName: 'emp_identificador',
         targetName: 'identificador',
@@ -20,6 +28,10 @@ module.exports = {
     }, {
         sourceName: 'usu_perfil',
         targetName: 'rol',
+        defaultValue: ''
+    }, {
+        sourceName: 'noContrato',
+        targetName: 'No Contrato',
         defaultValue: ''
     }]
 }
