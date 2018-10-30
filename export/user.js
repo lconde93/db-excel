8817a15985361e5b05f16e6df4b7273a35d3c6ab
Luis Conde <lconde@e-bitware.com>
2018-10-29 18:58:56 -0600

    [Estable] modificacion de archivo usuario

--- export/user.js
+++ export/user.js
@@ -1,37 +1,76 @@
 module.exports = {
     fileName: 'user',
-    rawQuery: 'SELECT * FROM usuario;SELECT * FROM acreditado;',
+    rawQuery: 'select acr_identificador, acr_nombre, acr_paterno, acr_materno, acr_telefono, acr_celular, acr_correo, acr_calle, acr_exterior, acr_interior, acr_colonia, acr_cp from acreditado;',
     transform: function(rows) {
-        let users = rows[0];
-        let acre = rows[1];
-
+        let users = [];
         let count = 0;
-        for(let item of users) {
-            item.usu_nombre += ' ' + (acre[count].acr_nombre ? acre[count].acr_nombre : 'No tiene prro');
-            item.noContrato = 'C00' + count;
-            count++;
+
+        for(let item of rows) {
+            if (item.acr_nombre != '-') {
+                if (item.acr_telefono == '-' || item.acr_telefono == '--' || item.acr_telefono == '000' || item.acr_telefono == '-' || item.acr_telefono == '0000000' || item.acr_telefono == '00000000000' || item.acr_telefono == '1' || item.acr_telefono == 'OOOOOO')
+                    item.acr_telefono = '';
+
+                if (item.acr_interior == 's/n' || item.acr_interior == 'S/N' || item.acr_interior == '--' || item.acr_interior == 'SN')
+                    item.acr_interior = '';
+                
+                item.noContrato = 'C00' + ++count;
+
+                users.push(item);
+            }
         }
         return users;
     },
     fields: [{
-        sourceName: 'emp_identificador',
+        sourceName: 'acr_identificador',
         targetName: 'identificador',
         defaultValue: ''
     }, {
-        sourceName: 'usu_nombre',
+        sourceName: 'acr_nombre',
         targetName: 'nombre',
         defaultValue: ''
     }, {
-        sourceName: 'usu_cuenta',
-        targetName: 'usuario',
+        sourceName: 'acr_paterno',
+        targetName: 'apellido_paterno',
         defaultValue: ''
     }, {
-        sourceName: 'usu_perfil',
-        targetName: 'rol',
+        sourceName: 'acr_materno',
+        targetName: 'apellido_materno',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_telefono',
+        targetName: 'telefono_local',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_celular',
+        targetName: 'celular',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_correo',
+        targetName: 'correo',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_calle',
+        targetName: 'calle',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_exterior',
+        targetName: 'no_exterior',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_interior',
+        targetName: 'no_interior',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_colonia',
+        targetName: 'colonia',
+        defaultValue: ''
+    }, {
+        sourceName: 'acr_cp',
+        targetName: 'cp',
         defaultValue: ''
     }, {
         sourceName: 'noContrato',
-        targetName: 'No Contrato',
+        targetName: 'no_contrato',
         defaultValue: ''
     }]
 }
