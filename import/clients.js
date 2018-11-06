48fe184dc5d4ebf63d7b86db911438560c69c12c
Alejandro Villarroel Calderon <AV90197@e-bitware.com>
2018-11-06 14:09:15 -0600

    Steps with row index

--- import/clients.js
+++ import/clients.js
@@ -1,17 +1,24 @@
 module.exports = {
 	fileName: '2018-11-06-Clientes.xlsx',
+	disabled: false,
 	fields: [/* {
 		sourceName: 'Id',
 		targetName: 'identif'
 	} */],
-	steps: [async function (db, row) {
-		const existsAdress = await db.query('select colonia_id from cat_colonias where colonia like "%' + row['Colonia'] + '%" and cp = ' + row['Codigo Postal']  +';');
+	steps: [async function (db, row, index) {
+		const existsAdress =
+			await db.query('select colonia_id from cat_colonias where colonia like "%' + row['Colonia'] + '%" and cp = ' + row['Codigo Postal'] + ';');
 
+		console.log('Step 1:', existsAdress);
 
-		return { keys: [] }
-	}, async function(db, row, keys) {
+		return { keys: [{ id: 1 }] };
+	}, async function (db, row, index, args) {
+		console.log('Step: 2', args);
 
-	}, async function(db, row, keys) {
+		return { hello: 1 };
+	}, async function (db, row, index, args) {
+		console.log('Step: 2', args, index);
 
+		return { keys: [] };
 	}],
 }
--- import/contracts.js
+++ import/contracts.js
@@ -1,3 +1,17 @@
 module.exports = {
+	fileName: '2018-11-01-Contratos.xlsx',
+	disabled: true,
+	fields: [],
+	steps: [async function (db, row) {
+		const existsProduct =
+			await db.query('select producto_id from cat_colonias where colonia like "%' + row['Colonia'] + '%" and cp = ' + row['Codigo Postal'] + ';');
 
+		console.log('10', existsAdress);
+
+		return { keys: [] }
+	}, async function (db, row, keys) {
+
+	}, async function (db, row, keys) {
+
+	}],
 }
