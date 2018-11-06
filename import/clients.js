1f88e94d832086b240c9b159c5d19d44e9beb52d
Alejandro Villarroel Calderon <AV90197@e-bitware.com>
2018-11-06 12:46:43 -0600

    Funcionality of import, steps ready

--- import/clients.js
+++ import/clients.js
@@ -1,7 +1,17 @@
 module.exports = {
-	path: '',
-	fields: [],
-	steps: [function (db) {
-		console.log(db.query);
+	fileName: '2018-11-06-Clientes.xlsx',
+	fields: [/* {
+		sourceName: 'Id',
+		targetName: 'identif'
+	} */],
+	steps: [async function (db, row) {
+		const existsAdress = await db.query('select colonia_id from cat_colonias where colonia like "%' + row['Colonia'] + '%" and cp = ' + row['Codigo Postal']  +';');
+
+
+		return { keys: [] }
+	}, async function(db, row, keys) {
+
+	}, async function(db, row, keys) {
+
 	}],
 }
